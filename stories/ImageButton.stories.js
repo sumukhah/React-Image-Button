4c47170607aaa6bb53d28d5a97cfd04fc80602ea
Sumukha Hegde <sumukha214@gmail.com>
2021-05-02 20:51:13 +0530

    First commit

---
+++ stories/ImageButton.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "./assets/image.jpeg";
+
+import ImageWithButtons from "../src/components";
+import { Button } from "./Button";
+
+export default {
+  title: "ImageButton",
+  component: ImageWithButtons,
+};
+
+const Template = (args) => (
+  <ImageWithButtons {...args}>
+    <Button primary label="Image-Button" />
+  </ImageWithButtons>
+);
+
+export const ZeroConfig = Template.bind({});
+ZeroConfig.args = {
+  img: <img src={Image} />,
+};
+
+export const EventListeners = Template.bind({});
+EventListeners.args = {
+  img: <img src={Image} />,
+  onClick: () => {console.log('you have clicked on the region')},
+  onMouseEnter: () => {console.log('Mouse is entered the region')},
+  onMouseLeave: () => {console.log('Mouse left the region')},
+};
+
+export const AllOtherProps = Template.bind({});
+AllOtherProps.args = {
+  img: <img src={Image} />,
+  zoomOnHover: -1,
+  animationDuration: 0.4,
+  buttonPosition: "bottom",
+  imageBackgroundColor: 'gray',
+  containerStyle: {borderRadius: '40px'},
+};
